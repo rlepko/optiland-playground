319d8a71dd2dbf580850f62b115eda84548a7117
rlepkowicz <rlepkowicz@gmail.com>
2025-07-20 22:15:46 +0000

    Working Optiland project with Dev Container

---
+++ reverse_telephoto_example.py
@@ -0,0 +1,9 @@
+import matplotlib
+matplotlib.use("Agg")  # Use non-GUI backend
+
+import matplotlib.pyplot as plt
+from optiland.samples.objectives import ReverseTelephoto
+
+lens = ReverseTelephoto()
+lens.draw() # This will not display in a non-GUI environment
+plt.savefig("lens_plot.png")  # Save instead of show
